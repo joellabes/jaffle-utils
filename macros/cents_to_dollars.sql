632b66a3fe9f42698bb4895129722acecf3e0554
Joel Labes <joel.labes@dbtlabs.com>
2026-04-08 16:19:29 +1200

    Fix dispatch

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,7 +1,7 @@
 {# A basic example for a project-wide macro to cast a column uniformly #}
 
 {% macro cents_to_dollars(column_name) -%}
-    {{ return(adapter.dispatch('cents_to_dollars')(column_name)) }}
+    {{ return(adapter.dispatch('cents_to_dollars', 'jaffle_utils')(column_name)) }}
 {%- endmacro %}
 
 {% macro default__cents_to_dollars(column_name) -%}
